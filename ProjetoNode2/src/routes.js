beaa68c2cce732c1ff96214ba19e46f1bbf92878
anteli1212 <guilherme.dotelilima@gmail.com>
2026-02-14 19:04:56 -0300

    Delete ProjetoNode2/src directory

--- ProjetoNode2/src/routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import userController from "./controllers/userController.js"
-const routes = express();
-
-routes.use("/user", userController);
-
-export default routes;
