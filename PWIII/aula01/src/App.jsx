3e21551423d5fd523a59543b8114e70da7e4d69e
Guilherme <guilherme.dotelilima@gmail.com>
2026-02-13 22:21:57 -0300

    Aula 1 de PWIII

---
+++ PWIII/aula01/src/App.jsx
@@ -0,0 +1,13 @@
+import FirstComponent from "./Components/FirstComponent"
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Este é o meu primeiro parágrafo</p>
+
+      <FirstComponent />
+    </div>
+  )
+}
---
+++ PWIII/aula01/src/Components/FirstComponent/index.jsx
@@ -0,0 +1,9 @@
+import "./FirstComponent.css"
+
+export default function FirstComponent(){
+    return(
+        <div className="titulo">
+            <h1 >Meu Primeiro Componente <b>React</b></h1>
+        </div>
+    )
+}
